5a772d76f4e04b72ef88accd54ba202e6138d66c
tuan anh <haostuananh1202@gmail.com>
2022-04-14 22:59:56 +0700

    tuan anh sua

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 </div>
 <footer class="alert alert-secondary" role="alert" text="text-danger" >
-        &#169; PBPTA - Team TFT (HUTECH)
+        &#169;tuan anh PBPTA - Team TFT (HUTECH)
     </footer>
 </body>
 </html>
